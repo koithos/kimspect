c3835a24fe4dc931dba9fa0b14cc467b59fcf994
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2025-03-28 20:54:38 +0100

    Added integration tests
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

---
+++ tests/integration_tests.rs
@@ -0,0 +1,29 @@
+use anyhow::Result;
+use kelper::k8s::K8sClient;
+
+#[tokio::test]
+async fn test_k8s_client_creation() -> Result<()> {
+    let client = K8sClient::new().await?;
+    assert!(client.is_initialized().await?);
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_get_pod_images() -> Result<()> {
+    let client = K8sClient::new().await?;
+    let _images = client.get_pod_images("default", None).await?;
+    // We can't assert specific values here as they depend on the cluster state
+    // but we can verify the function doesn't panic
+    assert!(true);
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_get_pod_images_with_node() -> Result<()> {
+    let client = K8sClient::new().await?;
+    let _images = client.get_pod_images("default", Some("node-name")).await?;
+    // We can't assert specific values here as they depend on the cluster state
+    // but we can verify the function doesn't panic
+    assert!(true);
+    Ok(())
+}
