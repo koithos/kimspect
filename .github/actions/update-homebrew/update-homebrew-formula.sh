c3430ee554994a3a3712317143b2199948dee1c4
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2025-04-06 22:58:47 +0200

    fix: updated homebrew release paths (#13)
    * Updated homebrew release paths
    
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>
    
    * Updated homebrew release paths
    
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>
    
    * Updated executable names
    
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>
    
    * Parametrized executable name to homebrew script
    
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>
    
    * Updated variable description
    
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>
    
    ---------
    
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- .github/actions/update-homebrew/update-homebrew-formula.sh
+++ .github/actions/update-homebrew/update-homebrew-formula.sh
@@ -7,12 +7,14 @@ RELEASE_TAG="$3"
 REPOSITORY="$4"
 AMD64_SHA="$5"
 ARM64_SHA="$6"
-COMMIT_MESSAGE="$7"
-TAP_TOKEN="$8" # Accept tap_token as an argument
+AMD64_NAME="$7"
+ARM64_NAME="$8"
+COMMIT_MESSAGE="$9"
+TAP_TOKEN="$10" # Accept tap_token as an argument
 
 TAP_DIR=$(basename "$TAP_REPO")
-AMD64_URL="https://github.com/${REPOSITORY}/releases/download/${RELEASE_TAG}/kelper-x86_64-apple-darwin"
-ARM64_URL="https://github.com/${REPOSITORY}/releases/download/${RELEASE_TAG}/kelper-aarch64-apple-darwin"
+AMD64_URL="https://github.com/${REPOSITORY}/releases/download/${RELEASE_TAG}/${AMD64_NAME}"
+ARM64_URL="https://github.com/${REPOSITORY}/releases/download/${RELEASE_TAG}/${ARM64_NAME}"
 
 echo "Cloning tap repository $TAP_REPO..."
 # Use the passed TAP_TOKEN for authentication
@@ -29,6 +31,9 @@ sed -i.bak "s#{{arm64_url}}#${ARM64_URL}#g" "$FORMULA_PATH"
 sed -i.bak "s#{{arm64_sha256}}#${ARM64_SHA}#g" "$FORMULA_PATH"
 rm "${FORMULA_PATH}.bak" # Remove backup files created by sed -i
 
+# Checkout main
+git checkout main
+
 echo "Committing and pushing changes..."
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
@@ -38,6 +43,7 @@ if git diff --staged --quiet; then
   echo "No changes to the formula file. Skipping commit."
 else
   git commit -m "$COMMIT_MESSAGE"
-  git push origin HEAD # Pushes to the default branch (usually main or master)
+  # Push changes
+  git push
   echo "Changes pushed successfully."
 fi
