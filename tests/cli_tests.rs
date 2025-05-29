f07131a5066ee3813ee55ed5f12682a84eef313e
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2025-05-29 17:32:07 +0200

    Updated tests
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -6,61 +6,58 @@ fn test_cli_parse_get_images_default() {
     let args = Args::parse_from(["kelper", "get", "images"]);
 
     let Commands::Get { resource } = args.command;
-    if let GetResource::Images {
+    let GetResource::Images {
         namespace,
         node,
         pod,
         registry,
         all_namespaces,
-    } = resource
-    {
-        assert_eq!(namespace, "default");
-        assert!(node.is_none());
-        assert!(pod.is_none());
-        assert!(registry.is_none());
-        assert!(!all_namespaces);
-    }
+    } = resource;
+
+    assert_eq!(namespace, "default");
+    assert!(node.is_none());
+    assert!(pod.is_none());
+    assert!(registry.is_none());
+    assert!(!all_namespaces);
 }
 
 #[test]
 fn test_cli_parse_get_images_namespace() {
     let args = Args::parse_from(["kelper", "get", "images", "--namespace", "test-ns"]);
     let Commands::Get { resource } = args.command;
-    if let GetResource::Images {
+    let GetResource::Images {
         namespace,
         node,
         pod,
         registry,
         all_namespaces,
-    } = resource
-    {
-        assert_eq!(namespace, "test-ns");
-        assert!(node.is_none());
-        assert!(pod.is_none());
-        assert!(registry.is_none());
-        assert!(!all_namespaces);
-    }
+    } = resource;
+
+    assert_eq!(namespace, "test-ns");
+    assert!(node.is_none());
+    assert!(pod.is_none());
+    assert!(registry.is_none());
+    assert!(!all_namespaces);
 }
 
 #[test]
 fn test_cli_parse_get_images_all_namespaces() {
     let args = Args::parse_from(["kelper", "get", "images", "--all-namespaces"]);
     let Commands::Get { resource } = args.command;
-    if let GetResource::Images {
+    let GetResource::Images {
         namespace,
         node,
         pod,
         registry,
         all_namespaces,
-    } = resource
-    {
-        // namespace should still be default, but all_namespaces flag should be true
-        assert_eq!(namespace, "default");
-        assert!(node.is_none());
-        assert!(pod.is_none());
-        assert!(registry.is_none());
-        assert!(all_namespaces);
-    }
+    } = resource;
+
+    // namespace should still be default, but all_namespaces flag should be true
+    assert_eq!(namespace, "default");
+    assert!(node.is_none());
+    assert!(pod.is_none());
+    assert!(registry.is_none());
+    assert!(all_namespaces);
 }
 
 #[test]
@@ -68,20 +65,19 @@ fn test_cli_parse_get_images_all_namespaces_short() {
     // Test the short flag version (-A)
     let args = Args::parse_from(["kelper", "get", "images", "-A"]);
     let Commands::Get { resource } = args.command;
-    if let GetResource::Images {
+    let GetResource::Images {
         namespace,
         node,
         pod,
         registry,
         all_namespaces,
-    } = resource
-    {
-        assert_eq!(namespace, "default");
-        assert!(node.is_none());
-        assert!(pod.is_none());
-        assert!(registry.is_none());
-        assert!(all_namespaces);
-    }
+    } = resource;
+
+    assert_eq!(namespace, "default");
+    assert!(node.is_none());
+    assert!(pod.is_none());
+    assert!(registry.is_none());
+    assert!(all_namespaces);
 }
 
 #[test]
@@ -89,20 +85,19 @@ fn test_cli_parse_get_images_node() {
     // Test combining node filter
     let args = Args::parse_from(["kelper", "get", "images", "--node", "worker1"]);
     let Commands::Get { resource } = args.command;
-    if let GetResource::Images {
+    let GetResource::Images {
         namespace,
         node,
         pod,
         registry,
         all_namespaces,
-    } = resource
-    {
-        assert_eq!(namespace, "default");
-        assert_eq!(node, Some("worker1".to_string()));
-        assert!(pod.is_none());
-        assert!(registry.is_none());
-        assert!(!all_namespaces);
-    }
+    } = resource;
+
+    assert_eq!(namespace, "default");
+    assert_eq!(node, Some("worker1".to_string()));
+    assert!(pod.is_none());
+    assert!(registry.is_none());
+    assert!(!all_namespaces);
 }
 
 #[test]
@@ -117,20 +112,19 @@ fn test_cli_parse_get_images_pod_and_all_namespaces() {
         "--all-namespaces",
     ]);
     let Commands::Get { resource } = args.command;
-    if let GetResource::Images {
+    let GetResource::Images {
         namespace,
         node,
         pod,
         registry,
         all_namespaces,
-    } = resource
-    {
-        assert_eq!(namespace, "default");
-        assert!(node.is_none());
-        assert_eq!(pod, Some("nginx-pod".to_string()));
-        assert!(registry.is_none());
-        assert!(all_namespaces);
-    }
+    } = resource;
+
+    assert_eq!(namespace, "default");
+    assert!(node.is_none());
+    assert_eq!(pod, Some("nginx-pod".to_string()));
+    assert!(registry.is_none());
+    assert!(all_namespaces);
 }
 
 #[test]
