ae180f9fd4b49649cc60acb164ee6dd4329bb460
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2025-06-22 21:13:34 +0200

    Updated tests
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -5,85 +5,76 @@ use kelper::{Args, Commands, GetImages, OutputFormat};
 fn test_cli_parse_get_images_default() {
     let args = Args::parse_from(["kelper", "get", "images"]);
 
-    if let Commands::Get { resource } = args.command {
-        if let GetImages::Images {
-            namespace,
-            node,
-            pod,
-            registry,
-            all_namespaces,
-            output,
-            kubeconfig: _,
-        } = resource
-        {
-            assert_eq!(namespace, "default");
-            assert!(node.is_none());
-            assert!(pod.is_none());
-            assert!(registry.is_none());
-            assert!(!all_namespaces);
-            assert_eq!(output, OutputFormat::Normal);
-        } else {
-            panic!("Expected GetImages::Images variant");
-        }
+    let Commands::Get { resource } = args.command;
+    if let GetImages::Images {
+        namespace,
+        node,
+        pod,
+        registry,
+        all_namespaces,
+        output,
+        kubeconfig: _,
+    } = resource
+    {
+        assert_eq!(namespace, "default");
+        assert!(node.is_none());
+        assert!(pod.is_none());
+        assert!(registry.is_none());
+        assert!(!all_namespaces);
+        assert_eq!(output, OutputFormat::Normal);
     } else {
-        panic!("Expected Commands::Get variant");
+        panic!("Expected GetImages::Images variant");
     }
 }
 
 #[test]
 fn test_cli_parse_get_images_namespace() {
     let args = Args::parse_from(["kelper", "get", "images", "--namespace", "test-ns"]);
-    if let Commands::Get { resource } = args.command {
-        if let GetImages::Images {
-            namespace,
-            node,
-            pod,
-            registry,
-            all_namespaces,
-            output,
-            kubeconfig: _,
-        } = resource
-        {
-            assert_eq!(namespace, "test-ns");
-            assert!(node.is_none());
-            assert!(pod.is_none());
-            assert!(registry.is_none());
-            assert!(!all_namespaces);
-            assert_eq!(output, OutputFormat::Normal);
-        } else {
-            panic!("Expected GetImages::Images variant");
-        }
+    let Commands::Get { resource } = args.command;
+    if let GetImages::Images {
+        namespace,
+        node,
+        pod,
+        registry,
+        all_namespaces,
+        output,
+        kubeconfig: _,
+    } = resource
+    {
+        assert_eq!(namespace, "test-ns");
+        assert!(node.is_none());
+        assert!(pod.is_none());
+        assert!(registry.is_none());
+        assert!(!all_namespaces);
+        assert_eq!(output, OutputFormat::Normal);
     } else {
-        panic!("Expected Commands::Get variant");
+        panic!("Expected GetImages::Images variant");
     }
 }
 
 #[test]
 fn test_cli_parse_get_images_all_namespaces() {
     let args = Args::parse_from(["kelper", "get", "images", "--all-namespaces"]);
-    if let Commands::Get { resource } = args.command {
-        if let GetImages::Images {
-            namespace,
-            node,
-            pod,
-            registry,
-            all_namespaces,
-            output,
-            kubeconfig: _,
-        } = resource
-        {
-            // namespace should still be default, but all_namespaces flag should be true
-            assert_eq!(namespace, "default");
-            assert!(node.is_none());
-            assert!(pod.is_none());
-            assert!(registry.is_none());
-            assert!(all_namespaces);
-            assert_eq!(output, OutputFormat::Normal);
-        } else {
-            panic!("Expected GetImages::Images variant");
-        }
+    let Commands::Get { resource } = args.command;
+    if let GetImages::Images {
+        namespace,
+        node,
+        pod,
+        registry,
+        all_namespaces,
+        output,
+        kubeconfig: _,
+    } = resource
+    {
+        // namespace should still be default, but all_namespaces flag should be true
+        assert_eq!(namespace, "default");
+        assert!(node.is_none());
+        assert!(pod.is_none());
+        assert!(registry.is_none());
+        assert!(all_namespaces);
+        assert_eq!(output, OutputFormat::Normal);
     } else {
-        panic!("Expected Commands::Get variant");
+        panic!("Expected GetImages::Images variant");
     }
 }
 
@@ -91,28 +82,25 @@ fn test_cli_parse_get_images_all_namespaces() {
 fn test_cli_parse_get_images_all_namespaces_short() {
     // Test the short flag version (-A)
     let args = Args::parse_from(["kelper", "get", "images", "-A"]);
-    if let Commands::Get { resource } = args.command {
-        if let GetImages::Images {
-            namespace,
-            node,
-            pod,
-            registry,
-            all_namespaces,
-            output,
-            kubeconfig: _,
-        } = resource
-        {
-            assert_eq!(namespace, "default");
-            assert!(node.is_none());
-            assert!(pod.is_none());
-            assert!(registry.is_none());
-            assert!(all_namespaces);
-            assert_eq!(output, OutputFormat::Normal);
-        } else {
-            panic!("Expected GetImages::Images variant");
-        }
+    let Commands::Get { resource } = args.command;
+    if let GetImages::Images {
+        namespace,
+        node,
+        pod,
+        registry,
+        all_namespaces,
+        output,
+        kubeconfig: _,
+    } = resource
+    {
+        assert_eq!(namespace, "default");
+        assert!(node.is_none());
+        assert!(pod.is_none());
+        assert!(registry.is_none());
+        assert!(all_namespaces);
+        assert_eq!(output, OutputFormat::Normal);
     } else {
-        panic!("Expected Commands::Get variant");
+        panic!("Expected GetImages::Images variant");
     }
 }
 
@@ -120,28 +108,25 @@ fn test_cli_parse_get_images_all_namespaces_short() {
 fn test_cli_parse_get_images_node() {
     // Test combining node filter
     let args = Args::parse_from(["kelper", "get", "images", "--node", "worker1"]);
-    if let Commands::Get { resource } = args.command {
-        if let GetImages::Images {
-            namespace,
-            node,
-            pod,
-            registry,
-            all_namespaces,
-            output,
-            kubeconfig: _,
-        } = resource
-        {
-            assert_eq!(namespace, "default");
-            assert_eq!(node, Some("worker1".to_string()));
-            assert!(pod.is_none());
-            assert!(registry.is_none());
-            assert!(!all_namespaces);
-            assert_eq!(output, OutputFormat::Normal);
-        } else {
-            panic!("Expected GetImages::Images variant");
-        }
+    let Commands::Get { resource } = args.command;
+    if let GetImages::Images {
+        namespace,
+        node,
+        pod,
+        registry,
+        all_namespaces,
+        output,
+        kubeconfig: _,
+    } = resource
+    {
+        assert_eq!(namespace, "default");
+        assert_eq!(node, Some("worker1".to_string()));
+        assert!(pod.is_none());
+        assert!(registry.is_none());
+        assert!(!all_namespaces);
+        assert_eq!(output, OutputFormat::Normal);
     } else {
-        panic!("Expected Commands::Get variant");
+        panic!("Expected GetImages::Images variant");
     }
 }
 
@@ -156,28 +141,25 @@ fn test_cli_parse_get_images_pod_and_all_namespaces() {
         "nginx-pod",
         "--all-namespaces",
     ]);
-    if let Commands::Get { resource } = args.command {
-        if let GetImages::Images {
-            namespace,
-            node,
-            pod,
-            registry,
-            all_namespaces,
-            output,
-            kubeconfig: _,
-        } = resource
-        {
-            assert_eq!(namespace, "default");
-            assert!(node.is_none());
-            assert_eq!(pod, Some("nginx-pod".to_string()));
-            assert!(registry.is_none());
-            assert!(all_namespaces);
-            assert_eq!(output, OutputFormat::Normal);
-        } else {
-            panic!("Expected GetImages::Images variant");
-        }
+    let Commands::Get { resource } = args.command;
+    if let GetImages::Images {
+        namespace,
+        node,
+        pod,
+        registry,
+        all_namespaces,
+        output,
+        kubeconfig: _,
+    } = resource
+    {
+        assert_eq!(namespace, "default");
+        assert!(node.is_none());
+        assert_eq!(pod, Some("nginx-pod".to_string()));
+        assert!(registry.is_none());
+        assert!(all_namespaces);
+        assert_eq!(output, OutputFormat::Normal);
     } else {
-        panic!("Expected Commands::Get variant");
+        panic!("Expected GetImages::Images variant");
     }
 }
 
@@ -185,28 +167,25 @@ fn test_cli_parse_get_images_pod_and_all_namespaces() {
 fn test_cli_parse_get_images_wide_output() {
     // Test wide output format
     let args = Args::parse_from(["kelper", "get", "images", "-o", "wide"]);
-    if let Commands::Get { resource } = args.command {
-        if let GetImages::Images {
-            namespace,
-            node,
-            pod,
-            registry,
-            all_namespaces,
-            output,
-            kubeconfig: _,
-        } = resource
-        {
-            assert_eq!(namespace, "default");
-            assert!(node.is_none());
-            assert!(pod.is_none());
-            assert!(registry.is_none());
-            assert!(!all_namespaces);
-            assert_eq!(output, OutputFormat::Wide);
-        } else {
-            panic!("Expected GetImages::Images variant");
-        }
+    let Commands::Get { resource } = args.command;
+    if let GetImages::Images {
+        namespace,
+        node,
+        pod,
+        registry,
+        all_namespaces,
+        output,
+        kubeconfig: _,
+    } = resource
+    {
+        assert_eq!(namespace, "default");
+        assert!(node.is_none());
+        assert!(pod.is_none());
+        assert!(registry.is_none());
+        assert!(!all_namespaces);
+        assert_eq!(output, OutputFormat::Wide);
     } else {
-        panic!("Expected Commands::Get variant");
+        panic!("Expected GetImages::Images variant");
     }
 }
 
@@ -214,28 +193,25 @@ fn test_cli_parse_get_images_wide_output() {
 fn test_cli_parse_get_images_wide_output_long() {
     // Test wide output format with long flag
     let args = Args::parse_from(["kelper", "get", "images", "--output", "wide"]);
-    if let Commands::Get { resource } = args.command {
-        if let GetImages::Images {
-            namespace,
-            node,
-            pod,
-            registry,
-            all_namespaces,
-            output,
-            kubeconfig: _,
-        } = resource
-        {
-            assert_eq!(namespace, "default");
-            assert!(node.is_none());
-            assert!(pod.is_none());
-            assert!(registry.is_none());
-            assert!(!all_namespaces);
-            assert_eq!(output, OutputFormat::Wide);
-        } else {
-            panic!("Expected GetImages::Images variant");
-        }
+    let Commands::Get { resource } = args.command;
+    if let GetImages::Images {
+        namespace,
+        node,
+        pod,
+        registry,
+        all_namespaces,
+        output,
+        kubeconfig: _,
+    } = resource
+    {
+        assert_eq!(namespace, "default");
+        assert!(node.is_none());
+        assert!(pod.is_none());
+        assert!(registry.is_none());
+        assert!(!all_namespaces);
+        assert_eq!(output, OutputFormat::Wide);
     } else {
-        panic!("Expected Commands::Get variant");
+        panic!("Expected GetImages::Images variant");
     }
 }
 
@@ -281,66 +257,57 @@ fn test_cli_parse_get_pods_namespace_and_all_namespaces_conflict() {
 #[test]
 fn test_cli_parse_get_registries_default() {
     let args = Args::parse_from(["kelper", "get", "registries"]);
-    if let Commands::Get { resource } = args.command {
-        if let GetImages::Registries {
-            namespace,
-            all_namespaces,
-            output,
-            kubeconfig: _,
-        } = resource
-        {
-            assert_eq!(namespace, "default");
-            assert!(!all_namespaces);
-            assert_eq!(output, OutputFormat::Normal);
-        } else {
-            panic!("Expected GetImages::Registries variant");
-        }
+    let Commands::Get { resource } = args.command;
+    if let GetImages::Registries {
+        namespace,
+        all_namespaces,
+        output,
+        kubeconfig: _,
+    } = resource
+    {
+        assert_eq!(namespace, "default");
+        assert!(!all_namespaces);
+        assert_eq!(output, OutputFormat::Normal);
     } else {
-        panic!("Expected Commands::Get variant");
+        panic!("Expected GetImages::Registries variant");
     }
 }
 
 #[test]
 fn test_cli_parse_get_registries_namespace() {
     let args = Args::parse_from(["kelper", "get", "registries", "--namespace", "test-ns"]);
-    if let Commands::Get { resource } = args.command {
-        if let GetImages::Registries {
-            namespace,
-            all_namespaces,
-            output,
-            kubeconfig: _,
-        } = resource
-        {
-            assert_eq!(namespace, "test-ns");
-            assert!(!all_namespaces);
-            assert_eq!(output, OutputFormat::Normal);
-        } else {
-            panic!("Expected GetImages::Registries variant");
-        }
+    let Commands::Get { resource } = args.command;
+    if let GetImages::Registries {
+        namespace,
+        all_namespaces,
+        output,
+        kubeconfig: _,
+    } = resource
+    {
+        assert_eq!(namespace, "test-ns");
+        assert!(!all_namespaces);
+        assert_eq!(output, OutputFormat::Normal);
     } else {
-        panic!("Expected Commands::Get variant");
+        panic!("Expected GetImages::Registries variant");
     }
 }
 
 #[test]
 fn test_cli_parse_get_registries_all_namespaces() {
     let args = Args::parse_from(["kelper", "get", "registries", "--all-namespaces"]);
-    if let Commands::Get { resource } = args.command {
-        if let GetImages::Registries {
-            namespace,
-            all_namespaces,
-            output,
-            kubeconfig: _,
-        } = resource
-        {
-            assert_eq!(namespace, "default");
-            assert!(all_namespaces);
-            assert_eq!(output, OutputFormat::Normal);
-        } else {
-            panic!("Expected GetImages::Registries variant");
-        }
+    let Commands::Get { resource } = args.command;
+    if let GetImages::Registries {
+        namespace,
+        all_namespaces,
+        output,
+        kubeconfig: _,
+    } = resource
+    {
+        assert_eq!(namespace, "default");
+        assert!(all_namespaces);
+        assert_eq!(output, OutputFormat::Normal);
     } else {
-        panic!("Expected Commands::Get variant");
+        panic!("Expected GetImages::Registries variant");
     }
 }
 
